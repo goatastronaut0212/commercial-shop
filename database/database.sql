7729563a2c03a8ddde1c9c1f2ff24d6eaf1d4a31
Goat Astronaut0212 <goatastronaut0212@proton.me>
2023-05-20 20:15:19 +0700

    Thêm bảng loại hàng và sản phẩm
     Changes to be committed:
    	new file:   database/database.sql

---
+++ database/database.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE mkjeandb;
+
+\c mkjeandb;
+
+CREATE TABLE category (
+    category_id    VARCHAR(20),
+    category_name  VARCHAR(100),
+    PRIMARY KEY(category_id)
+);
+
+CREATE TABLE product (
+    product_id     VARCHAR(20),
+    category_id    VARCHAR(20),
+    product_name   VARCHAR(20),
+    product_size   VARCHAR(20),
+    product_farbic VARCHAR(40),
+    product_price  INT,
+    product_detail VARCHAR(200),
+    PRIMARY KEY(product_id),
+    CONSTRAINT fk_category_id_for_product
+      FOREIGN KEY (category_id)
+      REFERENCES category(category_id)
+);
+
+CREATE TABLE product_image (
+    product_image_id VARCHAR(20),
+    product_id       VARCHAR(20),
+    product_image    BYTEA,
+    PRIMARY KEY(product_image_id),
+    CONSTRAINT fk_product_id_for_product_image
+        FOREIGN KEY (product_id)
+        REFERENCES product(product_id)
+);
