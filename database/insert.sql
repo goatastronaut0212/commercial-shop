00bbe3b58ebff254dfaec0e3b60dfa616bc2f6b5
Goat Astronaut0212 <goatastronaut0212@proton.me>
2023-05-27 19:27:34 +0700

    Sửa lại các quan hệ và dữ liệu, thêm drop database và insert dữ liệu

--- database/insert.sql
+++ database/insert.sql
@@ -3,4 +3,8 @@ INSERT INTO category VALUES
 ('2', 'Áo nam');
 
 INSERT INTO product VALUES
-('1', '1', 'Quần nam 1');
+('1', '1', 'Quần nam');
+
+INSERT INTO product_detail (product_detail_id, product_id) VALUES
+('XD32', '1'),
+('D33', '1')
